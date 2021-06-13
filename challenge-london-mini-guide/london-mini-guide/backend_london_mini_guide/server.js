44f41d1ab1d0462dc3c94b30fce60fe7128ae3e8
rfloresram <74856999+rfloresram@users.noreply.github.com>
2021-06-13 11:45:32 +0200

    Test1

--- challenge-london-mini-guide/london-mini-guide/backend_london_mini_guide/server.js
+++ challenge-london-mini-guide/london-mini-guide/backend_london_mini_guide/server.js
@@ -1,12 +1,12 @@
 const harrow = require("./data/Harrow.json");
-console.log(harrow)
+console.log(harrow);
 
 const express = require("express");
 const app = express();
 app.use(cors());
 
 app.get("/pharmacies", (request, response) => {
-    response.json(harrow.pharmacies);
+  response.json(harrow.pharmacies);
 });
 
 app.listen(5000);
